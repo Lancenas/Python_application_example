90ea4f436fb32167b3b8857e654eb0e6a2cb898c
lancenas <lxjyjs@gmail.com>
2023-07-25 12:22:14 +0800

    Create README.md

---
+++ Watermark/PDF批量处理添加文字水印.py
@@ -0,0 +1,116 @@
+import os
+import PyPDF2
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+def load_custom_font(font_path):
+    try:
+        pdfmetrics.registerFont(TTFont('CustomFont', font_path))
+        return True
+    except:
+        print("不支持该字体，请重新输入！")
+        return False
+
+def add_text_watermark(input_pdf, output_pdf, watermark_text, font_path, font_size=50, opacity=0.5, rotation=45, x_spacing=150, y_spacing=150):
+    # 加载自定义字体
+    if not load_custom_font(font_path):
+        return
+
+    # 打开PDF文件
+    pdf_reader = PyPDF2.PdfReader(input_pdf)
+    pdf_writer = PyPDF2.PdfWriter()
+
+    # 设置水印字体和大小
+    c = canvas.Canvas("watermark_tmp.pdf", pagesize=letter)
+    c.setFont('CustomFont', font_size)
+
+    # 设置水印透明度
+    c.setFillAlpha(opacity)
+
+    # 在每个位置添加水印
+    for x in range(0, int(c._pagesize[0]), x_spacing):
+        for y in range(0, int(c._pagesize[1]), y_spacing):
+            c.saveState()
+            c.translate(x, y)
+            c.rotate(rotation)
+            c.drawString(0, 0, watermark_text)
+            c.restoreState()
+
+    c.save()
+
+    # 打开水印PDF文件
+    watermark_reader = PyPDF2.PdfReader("watermark_tmp.pdf")
+    watermark_page = watermark_reader.pages[0]
+
+    # 给每一页加上水印
+    for page in pdf_reader.pages:
+        page.merge_page(watermark_page)
+        pdf_writer.add_page(page)
+
+    # 保存输出的PDF文件
+    with open(output_pdf, "wb") as output_file:
+        pdf_writer.write(output_file)
+
+    # 关闭文件
+    pdf_reader.stream.close()
+    watermark_reader.stream.close()
+
+    # 删除水印PDF文件
+    os.remove("watermark_tmp.pdf")
+
+if __name__ == "__main__":
+    while True:
+        folder_path = input("请输入要处理的文件夹路径：")
+        watermark_text = input("请输入水印文字：")
+        custom_font_path = input("请输入字体文件的路径（留空使用预置值“/Library/Fonts/Microsoft/SimHei.ttf”）：")
+        if not custom_font_path.strip():  # Check if input is empty
+            custom_font_path = "/Library/Fonts/Microsoft/SimHei.ttf"
+
+        font_size_input = input("请输入字体大小（留空使用预置值“24”）：")
+        if not font_size_input.strip():  # Check if input is empty
+            font_size = 24
+        else:
+            font_size = int(font_size_input)
+
+        opacity_input = input("请输入水印透明度（0-1之间）（留空使用预置值“0.2”）：")
+        if not opacity_input.strip():  # Check if input is empty
+            opacity = 0.2
+        else:
+            opacity = float(font_size_input)
+
+        rotation_input = input("请输入水印旋转角度（0-360之间）（留空使用预置值“30”）：")
+        if not rotation_input.strip():  # Check if input is empty
+            rotation = 30
+        else:
+            rotation = int(font_size_input)
+
+        x_spacing_input = input("请输入水印横向间距（留空使用预置值“150”）：")
+        if not x_spacing_input.strip():  # Check if input is empty
+            x_spacing = 150
+        else:
+            x_spacing = int(x_spacing_input)
+
+        y_spacing_input = input("请输入水印纵向间距（留空使用预置值“150”）：")
+        if not y_spacing_input.strip():  # Check if input is empty
+            y_spacing = 150
+        else:
+            y_spacing = int(y_spacing_input)
+
+        # 获取指定文件夹内的所有PDF文件
+        pdf_files = [f for f in os.listdir(folder_path) if f.lower().endswith('.pdf')]
+
+        for pdf_file in pdf_files:
+            input_pdf_filename = os.path.join(folder_path, pdf_file)
+            default_output_pdf_filename = input_pdf_filename.replace(".pdf", "_watermark.pdf")
+
+            # 添加水印
+            add_text_watermark(input_pdf_filename, default_output_pdf_filename, watermark_text, custom_font_path, font_size,
+                               opacity, rotation, x_spacing, y_spacing)
+
+        print("水印添加完成！")
+
+        choice = input("是否继续添加水印？（输入 'e' 退出，输入其他任意键继续）：")
+        if choice.lower() == 'e':
+            break
---
+++ 测试.py
@@ -0,0 +1,161 @@
+import os
+import subprocess
+from pydub import AudioSegment
+import time
+import sys
+import threading
+
+def get_audio_format(file_path):
+    _, ext = os.path.splitext(file_path)
+    return ext[1:]
+
+def convert_audio_with_pydub(source_file, target_format):
+    audio = AudioSegment.from_file(source_file)
+    target_file = os.path.splitext(source_file)[0] + '.' + target_format
+    # 指定更高的音频比特率（根据需要调整）
+    audio.export(target_file, format=target_format, bitrate='128k')
+    return target_file
+
+def convert_audio_with_ffmpeg(source_file, target_format):
+    target_file = os.path.splitext(source_file)[0] + '.' + target_format
+    command = ['ffmpeg', '-i', source_file, '-c:a', 'aac', '-strict', 'experimental', target_file]
+
+    try:
+        result = subprocess.run(command, check=True, capture_output=True, text=True)
+        if result.returncode == 0:
+            return target_file
+        else:
+            print(f"转换失败：{result.stderr}")
+            return None
+    except subprocess.CalledProcessError as e:
+        print(f"转换失败：{e}")
+        return None
+
+def convert_audio(source_file, target_format):
+    source_format = get_audio_format(source_file)
+    if source_format == target_format:
+        print("源格式和目标格式相同，无需转换。")
+        return None
+
+    try:
+        target_file = os.path.splitext(source_file)[0] + '.' + target_format
+        '''
+        # Check if the target file already exists
+        if os.path.exists(target_file):
+            overwrite = input(f"文件 '{target_file}' 已存在。是否删除并覆盖？(y/n): ")
+            if overwrite.lower() == 'y':
+                os.remove(target_file)
+            else:
+                print("转换已取消。")
+                return None
+        '''
+        if target_format == 'm4a':
+            return convert_audio_with_ffmpeg(source_file, target_format)
+        else:
+            return convert_audio_with_pydub(source_file, target_format)
+
+    except Exception as e:
+        print(f"转换失败：{e}")
+        return None
+
+def animate_loading_message(message, event):
+    frames = ["-", "\\\\", "|", "/"]
+    num_frames = len(frames)
+
+    while not event.is_set():
+        for i in range(num_frames):
+            sys.stdout.write(f"\r{message} {frames[i]}")
+            sys.stdout.flush()
+            time.sleep(0.2)
+
+def file_overwrite_prompt(target_file):
+    while True:
+        overwrite = input(f"文件 '{target_file}' 已存在。是否删除并覆盖？(y/n): ")
+        if overwrite.lower() == 'y':
+            return True
+        elif overwrite.lower() == 'n':
+            return False
+        else:
+            print("无效的输入，请输入 'y' 或 'n'.")
+
+def convert_audio_with_loading_animation(source_file, target_format):
+    global loading_completed
+    loading_completed = threading.Event()
+
+    loading_thread = threading.Thread(target=animate_loading_message, args=("请耐心等待，处理中 ------", loading_completed))
+    loading_thread.start()
+
+    try:
+        target_file = os.path.splitext(source_file)[0] + '.' + target_format
+
+        # Check if the target file already exists
+        while os.path.exists(target_file):
+            loading_completed.set()  # Pause the animation
+            if not file_overwrite_prompt(target_file):
+                print("转换已取消。")
+                loading_completed.clear()  # Resume the animation
+                loading_thread.join()  # Wait for the animation thread to finish
+                print("\r" + " " * len("请耐心等待，处理中 ------") + "\r", end="")  # Clear the loading animation
+                return
+            os.remove(target_file)
+
+        target_file = convert_audio(source_file, target_format)
+        if target_file:
+            loading_completed.set()  # Set the event to signal the animation thread to terminate
+            loading_thread.join()  # Wait for the animation thread to finish
+            print("\r" + " " * len("请耐心等待，处理中 ------") + "\r", end="")  # Clear the loading animation
+            print(f"文件已成功转换为 {target_format} 格式：{target_file}")
+    except Exception as e:
+        loading_completed.set()  # Set the event to signal the animation thread to terminate
+        loading_thread.join()  # Wait for the animation thread to finish
+        print("\r" + " " * len("请耐心等待，处理中 ------") + "\r", end="")  # Clear the loading animation
+        print(f"转换失败：{e}")
+
+def convert_audio_with_retry(source_file, target_format):
+    while True:
+        try:
+            convert_audio_with_loading_animation(source_file, target_format)
+            break
+        except KeyboardInterrupt:
+            print("\n转换已取消。")
+            break
+        except Exception as e:
+            print(f"转换失败：{e}")
+            retry = input("是否重试？(y/n): ")
+            if retry.lower() != 'y':
+                print("转换已取消。")
+                break
+
+
+def main():
+    print("欢迎使用音频格式转换工具！输入 'o' 可退出程序。")
+
+    while True:
+        source_file = input("请输入源文件名: ")
+        if source_file.lower() == 'o':
+            print("程序已退出。")
+            break
+
+        if not os.path.isfile(source_file):
+            print("文件不存在，请重新输入。")
+            continue
+
+        source_format = get_audio_format(source_file)
+        if not source_format:
+            print("无法识别源文件格式，请输入支持的音频格式。")
+            continue
+
+        target_format = input("请输入目标转换格式: ")
+        if target_format.lower() == 'o':
+            print("程序已退出。")
+            break
+
+        if target_format not in ['wav', 'mp3', 'flac', 'm4a']:
+            print("不支持的目标格式，请输入支持的音频格式。")
+            continue
+
+        convert_audio_with_loading_animation(source_file, target_format)
+
+
+if __name__ == "__main__":
+    main()
