644c204cba1403ecf18043abeea2dc229877c91c
lancenas <lxjyjs@gmail.com>
2023-07-26 16:52:27 +0800

    Create README.md

---
+++ Investment Analysis/IPO新股列表.py
@@ -0,0 +1,41 @@
+import tushare as ts
+import pandas as pd  # Don't forget to import the pandas library!
+
+# Replace 'YOUR_TUSHARE_API_KEY' with your Tushare API key (optional)
+# If you don't have an API key, you can still use the library but with limited access.
+ts.set_token('c96bfb91e26676f8127aa8e473c48af9403d091c971ed282baf4be68')
+
+# Initialize the Tushare pro API
+pro = ts.pro_api()
+
+def get_all_ipo_list():
+    # Set the initial offset and limit values
+    offset = 0
+    limit = 50  # You can adjust this value based on your needs, but 50 is the maximum per request
+
+    all_ipo_list = []
+
+    while True:
+        # Get IPO new stock listings for the current page
+        current_page_data = pro.new_share(start_date='20230701', end_date='20230831', offset=offset, limit=limit)
+
+        # If there is no data for the current page, break the loop
+        if current_page_data.empty:
+            break
+
+        # Append the current page data to the overall IPO list
+        all_ipo_list.append(current_page_data)
+
+        # Increment the offset for the next page
+        offset += limit
+
+    # Concatenate all the data frames to get the final result
+    all_ipo_list = pd.concat(all_ipo_list).reset_index(drop=True)
+
+    return all_ipo_list
+
+# Call the function to get all IPO new stock listings
+all_ipo_listings = get_all_ipo_list()
+
+# Display all IPO new stock listings
+print(all_ipo_listings)
---
+++ Investment Analysis/沪深股市实时行情分析.py
@@ -0,0 +1,61 @@
+import tushare as ts
+import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+
+def get_daily_quotes(stock_code, start_date, end_date):
+    ts.set_token('c96bfb91e26676f8127aa8e473c48af9403d091c971ed282baf4be68')
+    pro = ts.pro_api()
+    df = pro.daily(ts_code=stock_code, start_date=start_date, end_date=end_date)
+    if not df.empty:
+        return df
+    else:
+        return None
+
+
+def plot_daily_data(df):
+    if df is None:
+        print("No data available.")
+        return
+
+    df['trade_date'] = pd.to_datetime(df['trade_date'])
+    df.sort_values(by='trade_date', ascending=True, inplace=True)
+
+    plt.figure(figsize=(12, 6))
+    plt.plot(df['trade_date'], df['close'], label='Close', color='b')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Daily Stock Price for {}'.format(df['ts_code'][0]))
+    plt.legend()
+    plt.grid(True)
+
+    # Save the plot to a file (optional)
+    plt.savefig('daily_stock_price_plot.png')
+
+    plt.show()
+
+
+def save_to_excel(df):
+    if df is None:
+        print("No data available to save.")
+        return
+
+    # Save the DataFrame to an Excel file
+    df.to_excel('daily_stock_data.xlsx', index=False)
+    print("Daily stock data saved to 'daily_stock_data.xlsx'.")
+
+
+if __name__ == "__main__":
+    stock_code = input("Enter the stock code (e.g., '000001.SZ') or 'e' to exit: ")
+
+    while stock_code.lower() != 'e':
+        start_date = input("Enter the start date (YYYYMMDD): ")
+        end_date = input("Enter the end date (YYYYMMDD): ")
+
+        df = get_daily_quotes(stock_code, start_date, end_date)
+        plot_daily_data(df)
+        save_to_excel(df)
+
+        stock_code = input("Enter another stock code or 'e' to exit: ")
